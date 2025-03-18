893fd1b56d0d79b40db3ec1f97d07b02aa6de9e6
RS-HGS <144434850+RS-HGS@users.noreply.github.com>
2025-03-18 10:14:29 +0000

    Update Program.cs

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -8,7 +8,28 @@
             // Rewrite the algorithm in C# bellow fixing the error
             // Show the test outputs for the test table in the Readme
             // Put your evidence where it says Do the coding task in C#
-
+            int startyear = 0;
+		int endyear = 0;
+		int difference = -1;
+		bool validchoice = false;
+		do
+		{
+		 difference = -1;
+		Console.WriteLine("Enter a start year");
+		 startyear = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter an end year");
+		 endyear = Convert.ToInt32(Console.ReadLine());
+		   if (startyear > endyear)
+		   {Console.WriteLine("Start year must be before end year");}
+			else if (startyear > 2000)
+			{Console.WriteLine("Start year must be before 2000");}
+				else
+				{ validchoice = true;}
+		}
+		while
+			( validchoice == false);
+		difference = endyear - startyear;
+		Console.WriteLine(difference);
 
         }
     }
